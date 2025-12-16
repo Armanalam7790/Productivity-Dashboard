475990cc66beff04086dd64c02dacd7126271dfe
Armanalam7790 <alamarman7790@gmail.com>
2025-12-16 16:48:35 +0530

    todo

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-var allElem = document.querySelectorAll('.elem')
-var AllfullElems = document.querySelectorAll('.fullElem')
-
+function openFeaturs() {
+    var allElem = document.querySelectorAll('.elem')
+var fullElemPage = document.querySelectorAll('.fullElem')
+var fullElemPagebackbtn = document.querySelectorAll('.fullElem .back')
 
 allElem.forEach(function(elem) {
 
@@ -8,8 +9,22 @@ allElem.forEach(function(elem) {
         
 
     
-       AllfullElems[elem.id].style.display = "block"
+       fullElemPage[elem.id].style.display = "block"
        
         
     })
 })
+
+
+fullElemPagebackbtn.forEach(function(back) {
+//   console.log(back);
+  back.addEventListener('click', function() {
+    console.log(back.id);
+    fullElemPage[back.id].style.display = "none"
+    
+  })
+  
+    
+})
+}
+openFeaturs()
